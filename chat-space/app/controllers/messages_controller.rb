596e13069bcbaf13524903ee7ab7c68449c598f3
joma <1042829km@gmail.com>
2019-07-14 11:17:59 +0900

    console、binding.pryの削除

--- chat-space/app/controllers/messages_controller.rb
+++ chat-space/app/controllers/messages_controller.rb
@@ -4,19 +4,16 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
-    # binding.pry
   end
 
   def create
     @message = @group.messages.new(message_params)
     if @message.save!
-      # binding.pry
       respond_to do |format|
         format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました' }
         format.json
       end
     else
-      # binding.pry
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
       render :index
