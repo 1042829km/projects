118dcfda32c3bba66e3c532a1e0a85d1f94b3240
joma <1042829km@gmail.com>
2019-08-25 21:27:10 +0900

    Update unicorn.rb

--- chat-space/config/unicorn.rb
+++ chat-space/config/unicorn.rb
@@ -1,14 +1,14 @@
-app_path = File.expand_path('../../../../../', __FILE__)
+app_path = File.expand_path('../../../../', __FILE__)
 
 worker_processes 1
 # currentを指定
-working_directory "projects/#{app_path}/current/chat-space"
+working_directory "#{app_path}/current/chat-space"
 
 # それぞれ、sharedの中を参照するよう変更
-listen "projects/#{app_path}/shared/tmp/sockets/unicorn.sock"
-pid "projects/#{app_path}/shared/tmp/pids/unicorn.pid"
-stderr_path "projects/#{app_path}/shared/log/unicorn.stderr.log"
-stdout_path "projects/#{app_path}/shared/log/unicorn.stdout.log"
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
 #以下は応用的な設定なので説明は割愛
 
